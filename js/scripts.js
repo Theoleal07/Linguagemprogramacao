f5b824a3d8816f9232c19630ed7cc627893109eb
Samuel-dev-97 <samuelpssouza@gmail.com>
2025-02-26 20:37:20 -0300

    Adicionando button calcular

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 function seguir(){
     alert('Você agora está seguindo Samuel P. Souza')
 }
+//exercício 1 
+function exe1(){
+    //vamos usar DOM para recuperar os dados do HTML
+    //DOM - Document Object Model 
+    let nro1 = Number(document.getElementById("nro1").value);
+    let nro2 =  Number(document.getElementById("nro2").value);
+    //vamos fazer a subtração 
+    let sub = nro1 - nro2
+   // alert("A subtração é " + sub)
+   document.getElementById("sub").innerText = "O resultado é " + sub 
+}
